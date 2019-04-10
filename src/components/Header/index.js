6f5d26557a69cdefd6e49eccc3dbd924ffd722e4
Davi <davirodrigues8@gmail.com>
2019-04-10 12:03:19 -0300

    Corrige link header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import { Container } from './styles';
 
 const Header = ({ title, username }) => (
   <Container>
-    <div>
-      <a href="/" className="back">
-        Voltar
-      </a>
-    </div>
+    <Link to="/" className="back">
+      Voltar
+    </Link>
     <div>{title}</div>
     <div className="username">{username}</div>
   </Container>
