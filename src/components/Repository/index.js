4c6e52e6e83fb98e107227497994bc0aaeafe4fd
Davi <davirodrigues8@gmail.com>
2019-04-10 21:30:25 -0300

    Adiciona layout da pagina de meus repositories

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-
-// import { PropTypes } from 'prop-types';
-import { Container } from './styles';
+import { PropTypes } from 'prop-types';
+import { Container, Ball } from './styles';
 
 const Repository = ({ repository }) => (
   <Container>
-    <h2>{repository.name}</h2>
-    <p>{repository.description}</p>
-    <p>{repository.language}</p>
-    <p>{repository.pushed_at}</p>
+    <a className="title" href={repository.html_url} target="_blank" rel="noopener noreferrer">
+      {repository.name}
+    </a>
+    <p>{repository.description || 'Descrição não cadastrada'}</p>
+    <div className="language">
+      <Ball />
+      <span>{repository.language}</span>
+    </div>
+    <span className="pushed">Atualizado em: {repository.pushed_at}</span>
   </Container>
 );
 
+Repository.propTypes = {
+  repository: PropTypes.shape({
+    name: PropTypes.string,
+    html_url: PropTypes.string,
+    description: PropTypes.string.isRequired,
+    language: PropTypes.string,
+    pushed_at: PropTypes.string,
+  }).isRequired,
+};
 export default Repository;
--- src/components/Repository/styles.js
+++ src/components/Repository/styles.js
@@ -3,13 +3,50 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
   height: 100%;
   width: 600px;
   margin-top: 10px;
-  padding: 20px;
+  padding: 15px 30px;
   background: #fff;
   border: 1px solid #cccc;
   border-radius: 5px;
+
+  .title {
+    font-size: 20px;
+    font-weight: bold;
+    color: #24292e;
+    text-decoration: none;
+    padding-bottom: 5px;
+  }
+
+  p {
+    font-size: 16px;
+    color: #586069;
+    padding-bottom: 10px;
+  }
+
+  .language {
+    display: flex;
+    align-items: normal;
+    font-size: 14px;
+    color: #24292e;
+    padding-bottom: 5px;
+  }
+
+  .pushed {
+    color: #586069;
+    font-size: 12px;
+  }
+`;
+
+export const Ball = styled.span`
+  border-radius: 50%;
+  display: inline-block;
+  height: 12px;
+  position: relative;
+  top: 1px;
+  width: 12px;
+  background-color: #f1e05a;
+  margin-right: 5px;
 `;
